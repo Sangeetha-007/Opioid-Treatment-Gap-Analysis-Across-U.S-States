0790c9d058c0739a746cdc4208f7fedc95d64413
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2025-08-06 16:03:21 -0400

    Update opioid.py

--- opioid.py
+++ opioid.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import numpy as np
+import openpyxl
+
+
+dataframe1 = pd.read_excel('/Users/sangeetha/Downloads/SUDORS-Fatal-Overdose-Data.xlsx', sheet_name='Data')
+
+print(dataframe1)
