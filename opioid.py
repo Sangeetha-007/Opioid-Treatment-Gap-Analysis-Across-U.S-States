6314c3d8536b34d140fc51e08f4cc41ee41868d7
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2025-08-06 18:51:30 -0400

    Update opioid.py

--- opioid.py
+++ opioid.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 import openpyxl
 import plotly.express as px
+import us
+import plotly.io as pio
+pio.renderers.default = 'browser'
 
 
 df = pd.read_excel('/Users/sangeetha/Downloads/SUDORS-Fatal-Overdose-Data.xlsx', sheet_name='Data')
 #print(df[df['Jurisdiction'] == 'Overall'])
 
+print(df.isnull())
 #Filtering out Overall data
 df_filtered = df[df['Jurisdiction'] != 'Overall']
 
@@ -20,7 +24,22 @@ columns_to_drop = df_filtered.columns[190:275]  # 275 because the end index is e
 # Drop those columns
 df_filtered = df_filtered.drop(columns=columns_to_drop)
 
-
 columns_to_drop=df_filtered.columns[30:42]
 df_filtered=df_filtered.drop(columns=columns_to_drop)
 print(df_filtered)
+
+
+# Convert full state names to abbreviations
+df_filtered['Jurisdiction'] = df_filtered['Jurisdiction'].apply(lambda x: us.states.lookup(x).abbr if us.states.lookup(x) else x)
+
+df_2023 = df_filtered[df_filtered['year'] == 2023]
+
+
+fig = px.choropleth(df_2023,
+                        locations='Jurisdiction',
+                        locationmode="USA-states",
+                        scope="usa",
+                        color='opioids_rate',  # Column with the data to color the states
+                        color_continuous_scale="Viridis_r", # Choose a color scale
+                        title="Opioid Rate Per State in 2023")
+fig.show() 
